aa5871ddf8677d7b8b28dc6e5a6b1872273928f5
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-02-05 17:42:45 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>俊明が新たに作成</h1>
+      <h2>Start eto see some magic happen!</h2>
+    </div>
+  );
+}
